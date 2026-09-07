d29a5b546d60878f1b4de76bafb877d40c10a60b
Austin Rohl <austin@ajrpdrdt.com>
2026-09-07 18:49:05 -0500

    Add GA4 tag G-0KZLTYTHFS alongside existing Google Ads AW-18398757431

---
+++ site/js/analytics.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "AW-18398757431");
+gtag("config", "G-0KZLTYTHFS");
